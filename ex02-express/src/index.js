424ecb832cbd16c119284342ea057f42df866542
Marcio Bueno <marciobueno@gmail.com>
2024-10-03 19:49:18 -0300

    Aplicativo mínimo com Express

--- ex02-express/src/index.js
+++ ex02-express/src/index.js
@@ -1,5 +1,11 @@
 import "dotenv/config";
+import express from "express";
 
-console.log("Hello Project.");
+const app = express();
 
-console.log("MY_SECRET", process.env.MY_SECRET);
+app.get("/", (req, res) => {
+  res.send("Hello from Express!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
