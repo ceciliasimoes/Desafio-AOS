5be7b37987b09b23fbe90d6e09fd8eee712950ab
Marcio Bueno <marciobueno@gmail.com>
2024-10-07 20:01:12 -0300

    ex02 - aplicação Express + Router + Context

--- ex02-express/src/index.js
+++ ex02-express/src/index.js
@@ -1,11 +1,25 @@
 import "dotenv/config";
+import cors from "cors";
 import express from "express";
+import models from "./models";
+import routes from "./routes";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello from Express!");
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1],
+  };
+  next();
 });
+
+app.use("/session", routes.session);
+app.use("/users", routes.user);
+app.use("/messages", routes.message);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
