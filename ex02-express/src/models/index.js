dfe2e20a665c58f7d37c6cc11e4796088d87ec08
Marcio Bueno <marciobueno@gmail.com>
2024-10-24 19:11:01 -0300

    Corrigindo código de acesso ao model do sequelize.

--- ex02-express/src/models/index.js
+++ ex02-express/src/models/index.js
@@ -3,7 +3,7 @@ import Sequelize from "sequelize";
 import getUserModel from "./user";
 import getMessageModel from "./message";
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, { ssl: true });
 
 const models = {
   User: getUserModel(sequelize, Sequelize),
