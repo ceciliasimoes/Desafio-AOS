265ce094002ad7a236f61740edff92108d73dd39
AC <acsimoes446@gmail.com>
2024-11-26 19:22:48 -0300

    teste 3

--- ex03-react/src/main.jsx
+++ ex03-react/src/main.jsx
@@ -5,15 +5,19 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import "./index.css";
 import App from "./App.jsx";
 
+const domain = import.meta.env.VITE_APP_AUTH0_DOMAIN
+const clientId=import.meta.env.VITE_APP_AUTH0_CLIENT_ID
+const  audience=import.meta.env.VITE_APP_AUTH0_AUDIENCE
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Auth0Provider
-      domain={import.meta.env.VITE_APP_AUTH0_DOMAIN}
-      clientId={import.meta.env.VITE_APP_AUTH0_CLIENT_ID}
+      domain={domain}
+      clientId={clientId}
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}
-      audience={import.meta.env.VITE_APP_AUTH0_AUDIENCE}
+      audience={audience}
     >
       <App />
     </Auth0Provider>
