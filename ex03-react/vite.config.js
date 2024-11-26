12727b25ad7dc3e9643988178aa548e0e4ad5046
AC <acsimoes446@gmail.com>
2024-11-26 18:59:23 -0300

    fix: teste

--- ex03-react/vite.config.js
+++ ex03-react/vite.config.js
@@ -1,18 +1,8 @@
 /* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
-
-dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env.VITE_APP_AUTH0_DOMAIN': JSON.stringify(process.env.VITE_APP_AUTH0_DOMAIN),
-    'process.env.VITE_APP_AUTH0_CLIENT_ID': JSON.stringify(process.env.VITE_APP_AUTH0_CLIENT_ID),
-    'process.env.VITE_APP_AUTH0_AUDIENCE': JSON.stringify(process.env.VITE_APP_AUTH0_AUDIENCE),
-    'process.env.VITE_APP_REDIRECT_URI': JSON.stringify(process.env.VITE_APP_REDIRECT_URI),
-    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
-  },
 })
